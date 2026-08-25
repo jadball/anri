dffef69e181968d3b9fe532dce0c169016553053
James Ball <jadball@gmail.com>
2026-08-25 17:51:38 +0200

    Move splat to sandbox for now.

--- anri/fwd/__init__.py
+++ anri/fwd/__init__.py
@@ -17,28 +17,29 @@ from ._impl.scan import (
     propagate_cov_scan_all,
     propagate_cov_scan_all_grains,
 )
-from ._impl.splat import peak_to_pixels, peaks_to_pixels, prepare_gaussian_bin, sample_gaussian_bins
+
+# from ._impl.splat import peak_to_pixels, peaks_to_pixels, prepare_gaussian_bin, sample_gaussian_bins
 
 # fmt: off
 __all__ = [
-    "hkl_to_k_omega",
+    "get_centroid_box",
+    "get_centroid_box_all",
+    "get_centroid_box_all_grains",
+    "get_centroid_scan",
+    "get_centroid_scan_all",
+    "get_centroid_scan_all_grains",
     "get_cov_in",
+    "hkl_to_k_omega",
+#    "peak_to_pixels",
+#    "peaks_to_pixels",
+#    "prepare_gaussian_bin",
     "propagate_cov",
     "propagate_cov_box",
-    "propagate_cov_box_all_grains",
     "propagate_cov_box_all",
+    "propagate_cov_box_all_grains",
     "propagate_cov_scan",
-    "propagate_cov_scan_all_grains",
     "propagate_cov_scan_all",
-    "get_centroid_box",
-    "get_centroid_box_all_grains",
-    "get_centroid_box_all",
-    "get_centroid_scan",
-    "get_centroid_scan_all_grains",
-    "get_centroid_scan_all",
-    "prepare_gaussian_bin",
-    "sample_gaussian_bins",
-    "peak_to_pixels",
-    "peaks_to_pixels"
+    "propagate_cov_scan_all_grains",
+#    "sample_gaussian_bins"
 ]
 # fmt: on
